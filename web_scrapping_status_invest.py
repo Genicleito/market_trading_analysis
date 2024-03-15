630f529f97ff7ea331e0d4fd2a9d01250c54cf45
Genicleito Gonçalves <5203963+Genicleito@users.noreply.github.com>
2024-03-15 01:34:45 -0300

    Web Scraping Status Invest with Selenium

---
+++ web_scrapping_status_invest.py
@@ -0,0 +1,155 @@
+import os
+import time
+import json
+import pandas as pd
+from tqdm import tqdm
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service 
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+# https://selenium-python.readthedocs.io/installation.html#drivers
+# Drivers: https://googlechromelabs.github.io/chrome-for-testing/
+# https://storage.googleapis.com/chrome-for-testing-public/122.0.6261.128/win64/chromedriver-win64.zip
+
+#Instalando o driver
+# servico = Service(ChromeDriverManager().install())
+
+caminho_chromedriver = 'chromedriver.exe'
+# Crie uma instância do Service
+servico = webdriver.chrome.service.Service(caminho_chromedriver)
+
+#Abrindo o navegador
+navegador = webdriver.Chrome(service=servico)
+
+_FUNDOS = [
+	"MXRF11", "HGLG11", "KNRI11", "KNCR11", "XPML11"
+]
+_ACOES = [
+	'BBSE3', 'ITUB4', 'CSNA3', 'GGBR4', 'ITUB3', 'BBDC3',
+	'ITSA4', 'USIM5', 'CXSE3', 'KLBN4', 'TAEE4', 'SAPR4',
+]
+
+_URL = f"https://statusinvest.com.br"
+_PATH_FUNDOS = f"/fundos-imobiliarios"
+_PATH_ACOES = f"/acoes"
+
+_XPATHS = {
+	x: {
+		'segmento': '//*[@id="fund-section"]/div/div/div[4]/div/div[1]/div/div/div/a/strong'
+			if x in _FUNDOS else '//*[@id="company-section"]/div[1]/div/div[3]/div/div[3]/div/div/div/a/strong',
+		'tipo_anbima': '//*[@id="fund-section"]/div/div/div[2]/div/div[5]/div/div/div/strong'
+			if x in _FUNDOS else None,
+		'valor_atual': '//*[@id="main-2"]/div[2]/div[1]/div[1]/div/div[1]/strong'
+			if x in _FUNDOS else '//*[@id="main-2"]/div[2]/div/div[1]/div/div[1]/div/div[1]/strong',
+		'dividend_yield': '//*[@id="main-2"]/div[2]/div[1]/div[4]/div/div[1]/strong'
+			if x in _FUNDOS else '//*[@id="main-2"]/div[2]/div/div[1]/div/div[4]/div/div[1]/strong',
+		'pvp_acima_de_1_valorizado': '//*[@id="main-2"]/div[2]/div[5]/div/div[2]/div/div[1]/strong'
+			if x in _FUNDOS else '//*[@id="indicators-section"]/div[2]/div/div[1]/div/div[4]/div/div/strong',
+		'pebit_anos_retorno_investimento': '//*[@id="indicators-section"]/div[2]/div/div[1]/div/div[8]/div/div/strong'
+			if x in _ACOES else None,
+		'divida_liquida': '//*[@id="indicators-section"]/div[2]/div/div[2]/div/div[1]/div/div/strong'
+			if x in _ACOES else None,
+		'amount_dividendos_12meses': '//*[@id="main-2"]/div[2]/div/div[1]/div/div[4]/div/div[2]/div/span[2]'
+			if x in _ACOES else '//*[@id="main-2"]/div[2]/div[1]/div[4]/div/div[2]/div/span[2]'
+	} for x in _FUNDOS + _ACOES
+}
+
+
+indicadores = {
+	"ticker": [],
+	"segmento": [],
+	"tipo_anbima": [],
+	"valor_atual": [],
+	# "variacao_valor_atual": [],
+	"dividend_yield": [],
+	"pvp_acima_de_1_valorizado": [],
+	"pebit_anos_retorno_investimento": [],
+	"divida_liquida": [],
+	'preco_teto': [],
+}
+for ticker in tqdm(_ACOES + _FUNDOS):
+	# Criando url
+	url = f"{_URL}{_PATH_FUNDOS if ticker in _FUNDOS else _PATH_ACOES}/{ticker.lower()}"
+	
+	try:
+		# Abrindo o navegador
+		navegador.get(url)
+
+		time.sleep(1)
+
+		# Coletando dados
+		indicadores["ticker"].append(ticker)
+		if _XPATHS[ticker]['segmento']:
+			indicadores["segmento"].append(
+				navegador.find_element(By.XPATH, _XPATHS[ticker]['segmento']).text
+			)
+		else:
+			indicadores["segmento"].append(None)
+
+		if _XPATHS[ticker]['tipo_anbima']:
+			indicadores["tipo_anbima"].append(
+				navegador.find_element(By.XPATH, _XPATHS[ticker]['tipo_anbima']).text
+			)
+		else:
+			indicadores["tipo_anbima"].append(None)
+
+		if _XPATHS[ticker]['valor_atual']:		
+			indicadores["valor_atual"].append(
+				navegador.find_element(By.XPATH, _XPATHS[ticker]['valor_atual']).text.replace(',', '.')
+	    	)
+		else:
+			indicadores["valor_atual"].append(None)
+
+		if _XPATHS[ticker]['dividend_yield']:		
+			indicadores["dividend_yield"].append(
+				navegador.find_element(By.XPATH, _XPATHS[ticker]['dividend_yield']).text.replace(',', '.')
+	    	)
+		else:
+			indicadores["dividend_yield"].append(None)
+
+		if _XPATHS[ticker]['pvp_acima_de_1_valorizado']:		
+			indicadores["pvp_acima_de_1_valorizado"].append(
+				navegador.find_element(By.XPATH, _XPATHS[ticker]['pvp_acima_de_1_valorizado']).text.replace(',', '.')
+	    	)
+		else:
+			indicadores["pvp_acima_de_1_valorizado"].append(None)
+	
+		if _XPATHS[ticker]['pebit_anos_retorno_investimento']:		
+			indicadores["pebit_anos_retorno_investimento"].append(
+				navegador.find_element(By.XPATH, _XPATHS[ticker]['pebit_anos_retorno_investimento']).text.replace(',', '.')
+	    	)
+		else:
+			indicadores["pebit_anos_retorno_investimento"].append(None)
+
+		if _XPATHS[ticker]['divida_liquida']:		
+			indicadores["divida_liquida"].append(
+				navegador.find_element(By.XPATH, _XPATHS[ticker]['divida_liquida']).text.replace(',', '.')
+	    	)
+		else:
+			indicadores["divida_liquida"].append(None)
+
+		if _XPATHS[ticker]['amount_dividendos_12meses']:
+			indicadores['preco_teto'].append(
+				float(
+					navegador.find_element(By.XPATH, _XPATHS[ticker]['amount_dividendos_12meses']).text.replace(',', '.').replace('R$ ', '')
+				) / 0.06
+			)
+		else:
+			indicadores['preco_teto'].append(None)
+			
+	except Exception as exc:
+		print(exc)
+		navegador.close()
+		navegador.quit()
+
+# Fecha o navegador
+navegador.quit()
+
+df = pd.DataFrame(indicadores)
+df.head(15)
+
+# Write
+df.to_csv('indicadores-status-invest.csv', index=False)
